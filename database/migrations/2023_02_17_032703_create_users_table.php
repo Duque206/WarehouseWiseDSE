5659730fdbdd17af4ed727d85a90f3428f4e99cb
Duque206 <85969266+Duque206@users.noreply.github.com>
2024-08-17 11:51:04 -0600

    Migraciones de base de datos creadas

---
+++ database/migrations/2023_02_17_032703_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Role;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Warehouse::class)->nullable()->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->foreignIdFor(Role::class)->constrained();
+            $table->softDeletes();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
