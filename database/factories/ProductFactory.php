8974d38e2dc4285b2b27e162d803746a775dc026
Duque206 <85969266+Duque206@users.noreply.github.com>
2024-08-17 11:53:11 -0600

    Factories correspondientes a los modelos

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'uuid' => fake()->uuid(),
+            'name' => fake()->words(3, true),
+            'description' => fake()->paragraph(),
+            'unit_price' => fake()->randomFloat(2, 5, 150),
+        ];
+    }
+}
